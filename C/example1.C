d395493beae5db0974f0ab5a9192e852f4d1064a
John Burns <jzburns@gmail.com>
2024-08-11 16:20:11 +0000

    demo C code

---
+++ C/example1.C
@@ -0,0 +1,17 @@
+// compile this code using 
+// gcc example1.C
+// run the code using ./a.out
+#include <stdio.h>
+#include <stdint.h>
+
+int main (int argc, char** argv) {
+	uint32_t x = 1;
+	int32_t y = -1;
+
+	if (x > y) {
+		printf("correct!\n");
+	} else {
+		printf("something is wrong!\n");
+	}
+	return 0;
+}
