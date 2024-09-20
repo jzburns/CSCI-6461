e3176408090901e0ea9d5f483630ef7b4426e594
Dr. John Burns <jzburns@users.noreply.github.com>
2024-09-20 16:12:34 +0400

    Update flags.s

--- assembly/basic/flags.s
+++ assembly/basic/flags.s
@@ -1,11 +1,11 @@
 .global _start
 _start:
-	mov r4, #0x80000000
-	mov r5, #0x00000001
-	mov r11, #2
+	mov r2, #0x80000000
+	mov r3, #0x00000001
 	mov r7, #0
 	mov r8, #-5
 	mov r9, #7
+	mov r11, #2
 	
 	addeq r4, r5, #78	// Z=0
 	andhs r7, r8, r9	// C=1
