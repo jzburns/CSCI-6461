cadedd2908e73563f9e0769ebe8676f4572d3ef0
Dr. John Burns <jzburns@users.noreply.github.com>
2024-09-20 16:18:07 +0400

    Update flags.s

--- assembly/basic/flags.s
+++ assembly/basic/flags.s
@@ -1,5 +1,5 @@
 .global _start
-_start:
+_start:	
 	mov r2, #0x80000000
 	mov r3, #0x00000001
 	mov r7, #0
@@ -7,8 +7,12 @@ _start:
 	mov r9, #7
 	mov r11, #2
 	
- 	cmp r2, r3	// this operation will change the status flags
-	addeq r4, r5, #78	// Z=0
-	andhs r7, r8, r9	// C=1
-	orrmi r10, r11, r12	// N=0
-	eorlt r12, r7, r10	// N=0, V=1
+	// this operation will change the status flags
+ 	cmp r2, r3
+	
+	// now status flags are 0011
+	// what happens next?
+	addeq r4, r5, #78	//does this execute?
+	andhs r7, r8, r9	//does this?
+	orrmi r10, r11, r12	//does this?
+	eorlt r12, r7, r10	//does this?
