12135d3b644c213ab39a176848ba32cc9adb5183
Dr. John Burns <jzburns@users.noreply.github.com>
2024-09-21 17:09:14 +0400

    Update flags.s

--- assembly/basic/flags.s
+++ assembly/basic/flags.s
@@ -6,13 +6,14 @@ _start:
 	mov r8, #-5
 	mov r9, #7
 	mov r11, #2
+	mov r12, #0
 	
 	// this operation will change the status flags
  	// what is NZCV after this operation?
  	cmp r2, r3
 	
 	// which of these operations will execute?
-	addeq r4, r5, #78	//?
-	andhs r7, r8, r9	//?
-	orrmi r10, r11, r12	//?
-	eorlt r12, r7, r10	//?
+	addeq r4, r5, #78	//does this execute?
+	andhs r7, r8, r9	//does this?
+	orrmi r10, r11, r12	//does this?
+	eorlt r12, r7, r10	//does this?
